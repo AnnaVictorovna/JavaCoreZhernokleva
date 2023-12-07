57d138849005f84bd11a4b4e09c7c96164c8360e
Anna <madam.sestra@gmail.com>
2023-12-07 15:53:22 +0300

    9.b3 is ready for checking

---
+++ src/hometasks/lesson11/a/StudentException.java
@@ -0,0 +1,5 @@
+package hometasks.lesson11.a;
+
+public class StudentException extends Exception {
+
+}
---
+++ src/hometasks/lesson9/b1/Array.java
@@ -0,0 +1,63 @@
+package hometasks.lesson9.b1;
+
+import java.util.Arrays;
+
+public class Array<T> {
+    private int arrayLength = 10;
+    Object[] array = new Object[arrayLength];
+
+    public Object isArrayFullOrNot() {
+        int howManyFullPositionsInArray = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                howManyFullPositionsInArray++;
+            }
+        }
+        if (howManyFullPositionsInArray > 0) {
+            arrayLength = arrayLength * 2;
+        }
+        return array[arrayLength];
+    }
+
+    public void setElement(Object element) {
+        isArrayFullOrNot();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                array[i] = element;
+                break;
+            }
+        }
+    }
+
+    public void getElement(int index) {
+        System.out.println("Индексу " + index + " соответствует элемент " + array[index]);
+    }
+
+    public void contains(Object element) {
+        String contain = "Такой элемент НЕ содержится в массиве";
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                contain = "Такой элемент содержится в массиве";
+            }
+        }
+        System.out.println(contain);
+    }
+
+    public void getIndex(Object element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                System.out.println("Элемент " + element + " соответствует индексу " + i);
+            }
+        }
+    }
+
+    public void printArray() {
+        System.out.println("array = " + Arrays.toString(array));
+    }
+
+
+}
+/*3.Дополнить первое задание созданием функционала, который будет
+расширять вместимость массива в 2 раза, если при добавлении нового
+элемента в массив все ячейки уже заняты. Проверить работу этого
+метода.*/
