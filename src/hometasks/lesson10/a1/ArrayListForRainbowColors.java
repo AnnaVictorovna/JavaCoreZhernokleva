ddd61dfb452954417678a68d13ed745c274bea17
Anna <madam.sestra@gmail.com>
2023-12-11 14:21:50 +0300

    Level 10.a1 is ready for checking

---
+++ src/hometasks/lesson10/a1/ArrayListForRainbowColors.java
@@ -0,0 +1,31 @@
+package hometasks.lesson10.a1;
+
+import java.util.ArrayList;
+
+public class ArrayListForRainbowColors {
+    public static void main(String[] args) {
+        ArrayList<Colors> colors = new ArrayList<>();
+        colors.add(Colors.RED);
+        colors.add(Colors.ORANGE);
+        colors.add(Colors.YELLOW);
+        colors.add(Colors.GREEN);
+        colors.add(Colors.LIGHT_BLUE);
+        colors.add(Colors.BLUE);
+        colors.add(Colors.PURPLE);
+        System.out.println(colors);
+
+    }
+}
+
+enum Colors {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    LIGHT_BLUE,
+    BLUE,
+    PURPLE,
+    ;
+}
+/*Создать список ArrayList который хранит в себе все цвета радуги.
+Вывести их в консоль.*/
