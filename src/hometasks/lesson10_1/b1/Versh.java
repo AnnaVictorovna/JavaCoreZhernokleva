25dfa6f9a2574843219b693c94e65874cc89e878
Anna <madam.sestra@gmail.com>
2024-01-15 15:37:19 +0300

    10.1 b1 is ready

--- src/hometasks/lesson10_1/b1/Versh.java
+++
@@ -1,9 +0,0 @@
-package hometasks.lesson10_1.b1;
-
-public class Versh {
-    public static String versh =
-            "Трэба дома бываць часцей," +
-                    "Трэба дома бываць не госцем," +
-                    "Каб душою не ачарсцвець," +
-                    "Каб не страціць святое штосьці.";
-}
