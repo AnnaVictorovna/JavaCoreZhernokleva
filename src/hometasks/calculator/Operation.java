e39034f89ee60c9fb6a2b6260077cb9b4fa3f670
Anna <madam.sestra@gmail.com>
2024-01-19 11:31:02 +0300

    методы интерфейса теперь принимают не два параметра, а один

--- src/hometasks/calculator/Operation.java
+++ src/hometasks/calculator/Operation.java
@@ -1,13 +1,13 @@
 package hometasks.calculator;
 
 public interface Operation<T> {
-     T addition(T double1, T double2);
+     T addition(T double1);
 
-     T subtraction(T double1, T double2);
+     T subtraction(T double1);
 
-     T multiplication(T double1, T double2);
+     T multiplication(T double1);
 
-     T division(T double1, T double2);
+     T division(T double1);
 }
 /*3. Создайте интерфейс Operation с четырьмя методами для
 сложения, вычитания, умножения, деления значений. Каждый
