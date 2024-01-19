e8a24d116153efb8e2efa3a1bcd6a3976bd368ca
Anna <madam.sestra@gmail.com>
2024-01-19 11:18:39 +0300

    убрала реализацию в методах интерфейса

--- src/hometasks/calculator/Operation.java
+++ src/hometasks/calculator/Operation.java
@@ -1,21 +1,13 @@
 package hometasks.calculator;
 
 public interface Operation<T> {
-    default T addition(T double1, T double2) {
-        return null;
-    }
+     T addition(T double1, T double2);
 
-    default Double subtraction(Double double1, Double double2) {
-        return double1 - double2;
-    }
+     T subtraction(T double1, T double2);
 
-    default Double multiplication(Double double1, Double double2) {
-        return double1 * double2;
-    }
+     T multiplication(T double1, T double2);
 
-    default Double division(Double double1, Double double2) {
-        return double1 / double2;
-    }
+     T division(T double1, T double2);
 }
 /*3. Создайте интерфейс Operation с четырьмя методами для
 сложения, вычитания, умножения, деления значений. Каждый
