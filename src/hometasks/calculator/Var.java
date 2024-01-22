3e1a2c6b9caac7a8d1a90075e16124f28e57a937
Anna <madam.sestra@gmail.com>
2024-01-22 16:07:25 +0300

    дополнила калькулятор функцией сохранения в карту полученные от пользователь значения

--- src/hometasks/calculator/Var.java
+++ src/hometasks/calculator/Var.java
@@ -1,6 +1,18 @@
 package hometasks.calculator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Var {
+    static Map<String, String> map = new HashMap<>();
+
+    public static Map<String, String> saveKeyAndValue(String string) {
+        String[] parts = string.split("=");
+        String before = parts[0];
+        String after = parts[1];
+        map.put(before, after);
+        return map;
+    }
 }
 /*1.Создайте классы Scalar, Vector, Matrix, которые
 наследуются от абстрактного класса Var. для каждого класса
@@ -22,4 +34,8 @@ Matrix(String strMatrix)
 3. Создайте интерфейс Operation с четырьмя методами для
 сложения, вычитания, умножения, деления значений. Каждый
 метод на вход должен принимать один параметра с типом
-Scalar, Vector, Matrix и возвращать результат вычисления.*/
+Scalar, Vector, Matrix и возвращать результат вычисления.
+
+Дополните калькулятор функцией сохранения значений в переменные.
+Для сохранения значения в переменную используйте знак =. Пример
+ввода: А=3, В={1,2,3}.*/
