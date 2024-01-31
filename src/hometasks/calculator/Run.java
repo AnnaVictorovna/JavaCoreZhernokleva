aa719a0ec2298d0818dcac041809c122236bac4d
Anna <madam.sestra@gmail.com>
2024-01-31 14:06:04 +0300

    работает не только операция сложения, но и все остальные математические операции

--- src/hometasks/calculator/Run.java
+++ src/hometasks/calculator/Run.java
@@ -40,6 +40,72 @@ public class Run {
                         doublePlusDouble(stringsForKeys);
                     }
                 }
+            } else if (s.contains("-")) {
+                String[] stringsForNumbers = s.split("\\-");
+                if (isStringHasANumber(s, "\\-")) {
+                    if (stringsForNumbers[0].contains("{{")) {
+                        matrixMinusMatrix(stringsForNumbers);
+                    } else if (stringsForNumbers[0].contains("{")) {
+                        vectorMinusVector(stringsForNumbers);
+                    } else {
+                        doubleMinusDouble(stringsForNumbers);
+                    }
+                } else {
+                    String[] stringsForKeys = new String[2];
+                    stringsForKeys[0] = Var.map.get(stringsForNumbers[0]);
+                    stringsForKeys[1] = Var.map.get(stringsForNumbers[1]);
+                    if (stringsForKeys[0].contains("{{")) {
+                        matrixMinusMatrix(stringsForKeys);
+                    } else if (stringsForKeys[0].contains("{")) {
+                        vectorMinusVector(stringsForKeys);
+                    } else {
+                        doubleMinusDouble(stringsForKeys);
+                    }
+                }
+            } else if (s.contains("*")) {
+                String[] stringsForNumbers = s.split("\\*");
+                if (isStringHasANumber(s, "\\*")) {
+                    if (stringsForNumbers[0].contains("{{")) {
+                        matrixMultiplyMatrix(stringsForNumbers);
+                    } else if (stringsForNumbers[0].contains("{")) {
+                        vectorMultiplyVector(stringsForNumbers);
+                    } else {
+                        doubleMultiplyDouble(stringsForNumbers);
+                    }
+                } else {
+                    String[] stringsForKeys = new String[2];
+                    stringsForKeys[0] = Var.map.get(stringsForNumbers[0]);
+                    stringsForKeys[1] = Var.map.get(stringsForNumbers[1]);
+                    if (stringsForKeys[0].contains("{{")) {
+                        matrixMultiplyMatrix(stringsForKeys);
+                    } else if (stringsForKeys[0].contains("{")) {
+                        vectorMultiplyVector(stringsForKeys);
+                    } else {
+                        doubleMultiplyDouble(stringsForKeys);
+                    }
+                }
+            } else if (s.contains("/")) {
+                String[] stringsForNumbers = s.split("\\/");
+                if (isStringHasANumber(s, "\\/")) {
+                    if (stringsForNumbers[0].contains("{{")) {
+                        matrixDivideMatrix(stringsForNumbers);
+                    } else if (stringsForNumbers[0].contains("{")) {
+                        vectorDivideVector(stringsForNumbers);
+                    } else {
+                        doubleDivideDouble(stringsForNumbers);
+                    }
+                } else {
+                    String[] stringsForKeys = new String[2];
+                    stringsForKeys[0] = Var.map.get(stringsForNumbers[0]);
+                    stringsForKeys[1] = Var.map.get(stringsForNumbers[1]);
+                    if (stringsForKeys[0].contains("{{")) {
+                        matrixDivideMatrix(stringsForKeys);
+                    } else if (stringsForKeys[0].contains("{")) {
+                        vectorDivideVector(stringsForKeys);
+                    } else {
+                        doubleDivideDouble(stringsForKeys);
+                    }
+                }
             }
         }
     }
@@ -77,5 +143,84 @@ public class Run {
         System.out.println(d1 + d2);
     }
 
+    public static void matrixMinusMatrix(String[] strings) {
+        double[][] d1 = Matrix.convertStringToMatrixOfDoubles(strings[0]);
+        double[][] d2 = Matrix.convertStringToMatrixOfDoubles(strings[1]);
+        for (int i = 0; i < d1.length; i++) {
+            for (int j = 0; j < d1[i].length; j++) {
+                System.out.print(d1[i][j] - d2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void vectorMinusVector(String[] strings) {
+        double[] d1 = Vector.convertStringToDoubleArray(strings[0]);
+        double[] d2 = Vector.convertStringToDoubleArray(strings[1]);
+        double[] doubles = new double[d1.length];
+        for (int i = 0; i < d1.length; i++) {
+            doubles[i] = d1[i] - d2[i];
+            System.out.print(doubles[i] + " ");
+        }
+    }
+
+    public static void doubleMinusDouble(String[] strings) {
+        Double d1 = Double.parseDouble(strings[0]);
+        Double d2 = Double.parseDouble(strings[1]);
+        System.out.println(d1 - d2);
+    }
+
+    public static void matrixMultiplyMatrix(String[] strings) {
+        double[][] d1 = Matrix.convertStringToMatrixOfDoubles(strings[0]);
+        double[][] d2 = Matrix.convertStringToMatrixOfDoubles(strings[1]);
+        for (int i = 0; i < d1.length; i++) {
+            for (int j = 0; j < d1[i].length; j++) {
+                System.out.print(d1[i][j] * d2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void vectorMultiplyVector(String[] strings) {
+        double[] d1 = Vector.convertStringToDoubleArray(strings[0]);
+        double[] d2 = Vector.convertStringToDoubleArray(strings[1]);
+        double[] doubles = new double[d1.length];
+        for (int i = 0; i < d1.length; i++) {
+            doubles[i] = d1[i] * d2[i];
+            System.out.print(doubles[i] + " ");
+        }
+    }
+
+    public static void doubleMultiplyDouble(String[] strings) {
+        Double d1 = Double.parseDouble(strings[0]);
+        Double d2 = Double.parseDouble(strings[1]);
+        System.out.println(d1 * d2);
+    }
+
+    public static void matrixDivideMatrix(String[] strings) {
+        double[][] d1 = Matrix.convertStringToMatrixOfDoubles(strings[0]);
+        double[][] d2 = Matrix.convertStringToMatrixOfDoubles(strings[1]);
+        for (int i = 0; i < d1.length; i++) {
+            for (int j = 0; j < d1[i].length; j++) {
+                System.out.print(d1[i][j] / d2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void vectorDivideVector(String[] strings) {
+        double[] d1 = Vector.convertStringToDoubleArray(strings[0]);
+        double[] d2 = Vector.convertStringToDoubleArray(strings[1]);
+        double[] doubles = new double[d1.length];
+        for (int i = 0; i < d1.length; i++) {
+            doubles[i] = d1[i] / d2[i];
+            System.out.print(doubles[i] + " ");
+        }
+    }
+
+    public static void doubleDivideDouble(String[] strings) {
+        Double d1 = Double.parseDouble(strings[0]);
+        Double d2 = Double.parseDouble(strings[1]);
+        System.out.println(d1 / d2);
+    }
 }
