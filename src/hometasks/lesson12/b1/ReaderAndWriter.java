1989a2f6a2e8a8e745aac6ff51e2bb8d758a0d6f
Anna <madam.sestra@gmail.com>
2024-04-15 16:21:14 +0300

    12.B1 is ready for checking

--- src/hometasks/lesson12/b1/ReaderAndWriter.java
+++ src/hometasks/lesson12/b1/ReaderAndWriter.java
@@ -1,7 +1,17 @@
 package hometasks.lesson12.b1;
 
-public class ReaderAndWriter {
-    public static void readAndWrite(String oldFilePath, String newFilePath) {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
+public class ReaderAndWriter {
+    public static void writeStringInFile(String text, String fileName) {
+        try (Writer fileWriter = new FileWriter(fileName)) {
+            for (int i = 0; i < text.length(); i++) {
+                fileWriter.write(text.charAt(i));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/hometasks/lesson12/b1/Run.java
+++ src/hometasks/lesson12/b1/Run.java
@@ -1,8 +1,20 @@
 package hometasks.lesson12.b1;
 
-public class Run {
-    public static void main(String[] args) {
+import hometasks.lesson12.a1.Versh;
 
+public class Run {
+    public static final String FILE_PATH = "src/hometasks/lesson12/b1/versh.txt";
+    public static final String BUF_FILE_PATH = "src/hometasks/lesson12/b1/bufVersh";
+
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        ReaderAndWriter.writeStringInFile(Versh.versh, FILE_PATH);
+        long l = System.nanoTime() - start;
+        System.out.println(l);
+        long startBuf = System.nanoTime();
+        ReaderAndWriter.writeStringInFile(Versh.versh, BUF_FILE_PATH);
+        long lBuf = System.nanoTime() - startBuf;
+        System.out.println(lBuf);
     }
 }
 /*Напишите метод, который берёт стихотворение из класса Versh и
