49d2282e4d27180570ef34b1e4cec8bd507080eb
Anna <madam.sestra@gmail.com>
2024-05-06 15:51:02 +0300

    14.b, 17 a1, a2 are ready for checking

---
+++ src/hometasks/lesson17/а1/Run.java
@@ -0,0 +1,11 @@
+package hometasks.lesson17.а1;
+
+import java.time.LocalDateTime;
+
+public class Run {
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+}
+/*В методе main класса Run напишите код, который выводит в консоль
+текущее время, текущую дату.*/
---
+++ src/hometasks/lesson17/а2/Run.java
@@ -0,0 +1,14 @@
+package hometasks.lesson17.а2;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Run {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.of(2001, Month.MARCH, 23);
+        System.out.println(localDate);
+
+    }
+}
+/*В методе main класса Run создайте переменную, в которой хранится
+дата Вашего рождения. Выведите эту дату в консоль.*/
